d32fafdb1f221f3bfd980057df84665c4b71f1e8
Ernesto Ortiz <eortiz@skyknack.com>
2021-01-20 09:56:05 -0600

    partial commit.

--- Build/BuildPackage.ps1
+++ Build/BuildPackage.ps1
@@ -1,52 +1,53 @@
 ﻿$buildToolPath = "..\..\..\..\Bin\PX.CommandLine.exe"
 $vswherePath = ".\vswhere.exe"
-$webSitePath = "..\..\..\..\"
-$outPackagePath = ".\POCCI.zip"
-$binSourceFolder = "..\Builds\Temp\POCCILib\bin\Debug\*"
+$webSitePath = "C:\Projects\BlueIndigoBase\BlueIndigoBase"
+$outPackagePath = "..\Builds\POCCI.zip"
+$binSourceFolder = "..\Builds\Temp\POCCILib\bin\Release\*"
 $projectSourceFolder = "..\POCCILib\*"
 $tmpFolder = "..\Builds\Temp"
 $tmpBinFolder = "..\Builds\Temp\Bin"
 $solutionPath = "..\POCCILib\POCCILib.sln"
+$solutionPath2 = "..\POCCILib\POCCILib\POCCILib.csproj"
 
-$MSBuildPath = & $vswherePath -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
-if ($MSBuildPath) {
-  $MSBuildPath = join-path $MSBuildPath 'MSBuild\Current\Bin\MSBuild.exe'
-  if (test-path $MSBuildPath) {
-    Write-Host $MSBuildPath
-  }
-  else
-  {
-    Write-Host "Failed to find MSBuild"
-  }
-}
-else
-{
-    Write-Host "Failed to find MSBuild"
-}
+#$MSBuildPath = & $vswherePath -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+#if ($MSBuildPath) {
+#  $MSBuildPath = join-path $MSBuildPath 'MSBuild\Current\Bin\MSBuild.exe'
+#  if (test-path $MSBuildPath) {
+#    Write-Host $MSBuildPath
+#  }
+#  else
+#  {
+#    Write-Host "Failed to find MSBuild"
+#  }
+#}
+#else
+#{
+#    Write-Host "Failed to find MSBuild"
+#}
+#
+#set MSBuildEmitSolution=1
+#& $MSBuildPath $solutionPath2  /p:Configuration=Release /p:Platform="AnyCPU" /verbosity:minimal 
+#
+#echo "MSBuild called"
+#
+#if (!(Test-Path $tmpFolder))
+#{
+#    md $tmpFolder >$null 2>&1
+#}
+#else
+#{
+#    Remove-Item $tmpFolder -recurse -Exclude "Temp"    
+#}
+#
+#Copy-Item -Path $projectSourceFolder -Recurse -Destination $tmpFolder
+#
+#if (!(Test-Path $tmpBinFolder))
+#{
+#    md $tmpBinFolder >$null 2>&1
+#}
+#Copy-Item -Path $binSourceFolder -Recurse -Destination $tmpBinFolder
 
-echo "Call to msbuild with command $MSBuildPath $solutionPath /p:Configuration=Release /p:Platform=Any CPU /verbosity:minimal"
-
-& $MSBuildPath $solutionPath /p:Configuration=Release /p:Platform="Any CPU" /verbosity:minimal
-
-echo "MSBuild called"
-
-if (!(Test-Path $tmpFolder))
-{
-    md $tmpFolder >$null 2>&1
-}
-else
-{
-    Remove-Item $tmpFolder -recurse -Exclude "Temp"    
-}
-
-Copy-Item -Path $projectSourceFolder -Recurse -Destination $tmpFolder
-
-if (!(Test-Path $tmpBinFolder))
-{
-    md $tmpBinFolder >$null 2>&1
-}
-echo "Coing files"
-Copy-Item -Path $binSourceFolder -Recurse -Destination $tmpBinFolder
+echo   "$buildToolPath /method BuildProject /website $webSitePath /in $tmpFolder /out $outPackagePath /description Test"
 
 & $buildToolPath /method BuildProject /website $webSitePath /in $tmpFolder /out $outPackagePath /description "Test"
 
---
+++ POCCILib/POCCILib/docs/README.md
@@ -0,0 +1,9 @@
+﻿https://www.acumatica.com/media/2019/07/2019-Acumatica-DevCon-Source-Control-Automation.pdf
+
+https://asiablog.acumatica.com/2018/07/continues-integration-with-acumatica-platform.html
+
+https://stackoverflow.com/questions/13915636/specify-project-file-of-a-solution-using-msbuild
+
+https://docs.microsoft.com/en-us/visualstudio/msbuild/walkthrough-using-msbuild?view=vs-2019
+
+https://github.com/Acumatica/CustomizationBuildScript
